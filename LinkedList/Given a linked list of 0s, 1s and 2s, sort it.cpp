81b599e38cf2585353eaf1b1f1e4e33b7f5b957f
PULKIT <77159492+pulkitsupple@users.noreply.github.com>
2023-07-23 07:59:39 +0530

    Update Given a linked list of 0s, 1s and 2s, sort it.cpp

--- LinkedList/Given a linked list of 0s, 1s and 2s, sort it.cpp
+++ LinkedList/Given a linked list of 0s, 1s and 2s, sort it.cpp
@@ -2,37 +2,37 @@ class Solution
 {
     public:
     //Function to sort a linked list of 0s, 1s and 2s.
-    Node* segregate(Node *head) {
+   Node* segregate(Node *head) {
         
         // Add code here
         int zero = 0;
         int one = 0;
-        int two = 0;
-        
+        int two = 0;  
         Node *temp = head;
         
-        while(temp!=NULL){
+        while(temp){
             if(temp->data==0) zero++;
             else if(temp->data==1) one++;
             else two++;
             temp = temp->next;
         }
         
+        
         temp = head;
         
-        for(int i=0; i<zero; i++){
-            temp->data = 0;
-            temp = temp->next;
-        }
-        
-        
-        for(int i=zero; i<zero+one; i++){
-            temp->data = 1;
-            temp = temp->next;
-        }
-        
-        for(int i=zero+one; i<zero+one+two; i++){
-            temp->data = 2;
+        while(temp){
+            if(zero!=0){
+                temp->data = 0;
+                zero--;
+            }
+            else if(one!=0){
+                temp->data = 1;
+                one--;
+            }
+            else {
+                temp->data = 2;
+                two--;
+            }
             temp = temp->next;
         }
         
